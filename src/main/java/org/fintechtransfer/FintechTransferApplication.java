bab9effebb6b61a099b403a15efa51e812dacd73
Paul <paulukhov@gmail.com>
2026-08-19 21:26:53 +0500

    Initial commit: fintech-transfer

---
+++ src/main/java/org/fintechtransfer/FintechTransferApplication.java
@@ -0,0 +1,13 @@
+package org.fintechtransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechTransferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintechTransferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fintechtransfer/FintechTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fintechtransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechTransferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
