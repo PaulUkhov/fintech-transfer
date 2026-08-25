a66e5566f0d51b4e4ffbdb29d2b4cb73e54b3376
Alexey Krupnov <alexey.krupnov@gmail.com>
2026-08-25 19:11:08 +0300

    поправил чуть pom

--- src/main/java/org/fintechtransfer/model/ExchangeRateEntity.java
+++ src/main/java/org/fintechtransfer/model/ExchangeRateEntity.java
@@ -1,11 +1,16 @@
 package org.fintechtransfer.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.fintechtransfer.config.Auditable;
-
 import java.math.BigDecimal;
 import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "exchange_rates")
